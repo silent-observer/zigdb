0e7b5e16a4fd6d4f1b195f9e6673af70dff3e0aa
Maksim Michkov <m.michkov@arenadata.io>
2026-05-05 00:00:00 +0300

    Move everything else into server

--- build.zig
+++ build.zig
@@ -28,7 +28,7 @@ pub fn build(b: *std.Build) void {
         // intend to expose to consumers that were defined in other files part
         // of this module, you will have to make sure to re-export them from
         // the root file.
-        .root_source_file = b.path("src/common.zig"),
+        .root_source_file = b.path("src/common/common.zig"),
         // Later on we'll use this module as the root module of a test executable
         // which requires us to specify a target.
         .target = target,
@@ -48,7 +48,7 @@ pub fn build(b: *std.Build) void {
         // intend to expose to consumers that were defined in other files part
         // of this module, you will have to make sure to re-export them from
         // the root file.
-        .root_source_file = b.path("src/root.zig"),
+        .root_source_file = b.path("src/server/root.zig"),
         // Later on we'll use this module as the root module of a test executable
         // which requires us to specify a target.
         .target = target,
@@ -79,7 +79,7 @@ pub fn build(b: *std.Build) void {
             // b.createModule defines a new module just like b.addModule but,
             // unlike b.addModule, it does not expose the module to consumers of
             // this package, which is why in this case we don't have to give it a name.
-            .root_source_file = b.path("src/main.zig"),
+            .root_source_file = b.path("src/server/main.zig"),
             // Target and optimization levels must be explicitly wired in when
             // defining an executable or library (in the root module), and you
             // can also hardcode a specific target for an executable or library
@@ -108,7 +108,7 @@ pub fn build(b: *std.Build) void {
             // b.createModule defines a new module just like b.addModule but,
             // unlike b.addModule, it does not expose the module to consumers of
             // this package, which is why in this case we don't have to give it a name.
-            .root_source_file = b.path("src/main.zig"),
+            .root_source_file = b.path("src/server/main.zig"),
             // Target and optimization levels must be explicitly wired in when
             // defining an executable or library (in the root module), and you
             // can also hardcode a specific target for an executable or library
--- src/common/common.zig
+++ src/common/common.zig
@@ -1,14 +1,14 @@
-pub const types = @import("common/types.zig");
+pub const types = @import("types.zig");
 pub const DBType = types.DBType;
 pub const AttributeDescriptor = types.AttributeDescriptor;
 pub const TupleDescriptor = types.TupleDescriptor;
 
-pub const tuple = @import("common/tuple.zig");
+pub const tuple = @import("tuple.zig");
 pub const MemTuple = tuple.MemTuple;
 
-pub const value = @import("common/value.zig");
+pub const value = @import("value.zig");
 pub const Value = value.Value;
 pub const TypedValue = value.TypedValue;
-pub const oom = @import("common/utils.zig").oom;
+pub const oom = @import("utils.zig").oom;
 
-pub const ids = @import("common/ids.zig");
+pub const ids = @import("ids.zig");
